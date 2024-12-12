1d332d64a5dca17fd0347e0ff8611cbb09bbfc25
felippeximenes <felippelpximenes@outlook.com>
2024-12-12 15:27:32 -0300

    fazendo a leitura de dados para oprganização

---
+++ pipeline_dados/scripts/fusao_mercado_fev.py
@@ -0,0 +1,37 @@
+import json
+import csv
+
+
+def leitura_json(path_json):
+    dados_json = []
+    with open(path_json, 'r') as file:
+        dados_json = json.load(file)
+    return dados_json
+
+def leitura_csv(path_csv):
+    dados_csv = []
+    with open(path_csv, 'r') as file:
+        spamreader = csv.DictReader(file, delimiter = ',')
+        for row in spamreader:
+            dados_csv.append(row)
+    return dados_csv
+
+def leitura_dados(path, tipo_arquivo):
+    dados = []
+    if tipo_arquivo == 'csv':
+        dados = leitura_csv(path)
+    elif tipo_arquivo == 'json':
+        dados = leitura_json(path)
+    
+    return dados
+
+
+path_json = 'pipeline_dados/data_raw/dados_empresaA.json'
+path_csv = 'pipeline_dados/data_raw/dados_empresaB.csv'
+
+
+dados_json = leitura_json(path_json)
+print(dados_json[0])
+
+dados_csv = leitura_csv(path_csv)
+print(dados_csv[0])
