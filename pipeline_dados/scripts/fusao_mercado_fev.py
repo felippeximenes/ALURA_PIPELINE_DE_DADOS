1dbe0b1d01013f3fa6377942c83d8ece7013fea4
felippeximenes <felippelpximenes@outlook.com>
2024-12-13 15:46:42 -0300

    Inicio no processo de unir os dados

--- pipeline_dados/scripts/fusao_mercado_fev.py
+++ pipeline_dados/scripts/fusao_mercado_fev.py
@@ -1,5 +1,6 @@
 import json
 import csv
+from processamento_dados import Dados
 
 def leitura_json(path_json):
     dados_json = []
@@ -71,46 +72,48 @@ def salvando_dados(dados, path):
 path_json = 'pipeline_dados/data_raw/dados_empresaA.json'
 path_csv = 'pipeline_dados/data_raw/dados_empresaB.csv'
 
+dados_empresaA = Dados(path_json, 'json')
+print(dados_empresaA.path)
 
 # Iniciando a leitura
-dados_json = leitura_dados(path_json,'json')
-nome_colunas_json = get_columns(dados_json)
-tamanho_dados_json = size_data(dados_json)
+#dados_json = leitura_dados(path_json,'json')
+#nome_colunas_json = get_columns(dados_json)
+#tamanho_dados_json = size_data(dados_json)
 
-print(f"Nome colunas dados json: {nome_colunas_json}")
-print(f"Tamanho dos dados json: {tamanho_dados_json}")
+#print(f"Nome colunas dados json: {nome_colunas_json}")
+#print(f"Tamanho dos dados json: {tamanho_dados_json}")
 
-dados_csv = leitura_dados(path_csv, 'csv')
-nome_colunas_csv = get_columns(dados_csv)
-tamanho_dados_csv = size_data(dados_csv)
-print(nome_colunas_csv)
-print(tamanho_dados_csv)
+#dados_csv = leitura_dados(path_csv, 'csv')
+#nome_colunas_csv = get_columns(dados_csv)
+#tamanho_dados_csv = size_data(dados_csv)
+#print(nome_colunas_csv)
+#print(tamanho_dados_csv)
 
 #Transformacao dos dados
 
-key_mapping = {'Nome do Item': 'Nome do Produto',
-                'Classificação do Produto': 'Categoria do Produto',
-                'Valor em Reais (R$)': 'Preço do Produto (R$)',
-                'Quantidade em Estoque': 'Quantidade em Estoque',
-                'Nome da Loja': 'Filial',
-                'Data da Venda': 'Data da Venda'}
-dados_csv = rename_columns(dados_csv, key_mapping)
-nome_colunas_csv = get_columns(dados_csv)
-print(nome_colunas_csv)
+#key_mapping = {'Nome do Item': 'Nome do Produto',
+#                'Classificação do Produto': 'Categoria do Produto',
+#                'Valor em Reais (R$)': 'Preço do Produto (R$)',
+#                'Quantidade em Estoque': 'Quantidade em Estoque',
+#                'Nome da Loja': 'Filial',
+#                'Data da Venda': 'Data da Venda'}
+#dados_csv = rename_columns(dados_csv, key_mapping)
+#nome_colunas_csv = get_columns(dados_csv)
+#print(nome_colunas_csv)
 
-dados_fusao = join(dados_json, dados_csv)
-nome_colunas_fusao = get_columns(dados_fusao)
-tamanho_dados_fusao = size_data(dados_fusao)
-print(nome_colunas_fusao)
-print(tamanho_dados_fusao)
+#dados_fusao = join(dados_json, dados_csv)
+#nome_colunas_fusao = get_columns(dados_fusao)
+#tamanho_dados_fusao = size_data(dados_fusao)
+#print(nome_colunas_fusao)
+#print(tamanho_dados_fusao)
 
 
 #Salvando dados
 
-dados_fusao_tabela = transformando_dados_tabela(dados_fusao, nome_colunas_fusao)
+#dados_fusao_tabela = transformando_dados_tabela(dados_fusao, nome_colunas_fusao)
 
-path_dados_combinados = 'pipeline_dados/data_processed/dados_combinados.csv'
+#path_dados_combinados = 'pipeline_dados/data_processed/dados_combinados.csv'
 
-salvando_dados(dados_fusao_tabela, path_dados_combinados)
+#salvando_dados(dados_fusao_tabela, path_dados_combinados)
 
-print(path_dados_combinados)
+#print(path_dados_combinados)
---
+++ pipeline_dados/scripts/processamento_dados.py
@@ -0,0 +1,5 @@
+class Dados:
+
+    def __init__(self, path, tipo_dados):
+        self.path = path
+        self.tipo_arquivo = tipo_dados
